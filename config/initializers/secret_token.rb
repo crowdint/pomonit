147df3970bf5dea295248cd565cf7642c1fa7a50
Chalo Fernandez <chalofa@gmail.com>
2012-10-10 13:58:13 -0500

    Initial files load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pomonit::Application.config.secret_token = '1d2b78d5af71f482185ce4605b9c12cc330806e53972de971dc817105239b644bc0e625f95edf0ceff7c3b25fd89c3a0cc970e95aec7753dac4e32f1e60ea49d'
